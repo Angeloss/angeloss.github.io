498ff26f2739d0de07b49c6adef06e5252237e5d
Angeloss <angeloss49@gmail.com>
2026-01-22 11:22:10 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c275632678ab42a7c5b7.js"
+    "url": "webpack-runtime-c3599f23a9cc77deb6b3.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9d2ab5c2a01b5f6f75776a26a030da36"
+    "revision": "87b5b84de790d89009f30576a575b55f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
